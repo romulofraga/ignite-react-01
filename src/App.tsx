8e69e30a5c9a0f4f5ec3c19fa4bbd5cbfe5e11d9
Rômulo Ferreira Fraga <romulo.fraga.dev@gmail.com>
2021-06-10 23:32:42 -0300

    Listagem de repositórios do Github

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './styles/global.scss'
 import { RepositoryList } from './components/RepositoryList'
-import { Contador } from './components/Contador'
 
 export function App() { 
   return (
     <>
       <RepositoryList />
-      <Contador />
     </>
   )
 }
